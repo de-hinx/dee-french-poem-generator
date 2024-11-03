63850d4da6c6b9a1e1d9c361d0ebf40f0cf34507
Divine <divineanyanwu123@gmail.com>
2024-11-03 15:18:04 +0100

    created a search function and a function to generate the ai answers

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function search(event) {
+  event.preventDefault();
+  let searching = document.querySelector("#search-input");
+  generate(searching.value);
+}
 
+function generate(input) {
+  let key = `77bdfc047f054f23dabe6bdof740t2a3`;
+  let prompt = `generate a poem about ${input}`;
+  let context = `you are an AI with vast knowledge in french and poetry, write a poem that is short, precise and lovely. let there be spacing between each line of the poem`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+  console.log(input);
+  axios.get(apiUrl).then(poem);
+}
+function poem(lovely) {
+  console.log("processing");
+
+  new Typewriter(".poem", {
+    strings: `${lovely.data.answer}`,
+    autoStart: true,
+    cursor: null,
+    delay: 10,
+  });
+}
+let submitButton = document.querySelector("form");
+submitButton.addEventListener("submit", search);
